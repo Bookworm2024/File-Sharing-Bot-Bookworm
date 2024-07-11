a5b758d62b1ccf5504105aa63b4beb6cb2544728
Bookworm2024 <infohubnetworks@gmail.com>
2024-07-11 16:20:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7000091630:AAGn1-7jJP_6kS3gnGSfAjjsuOzDeJ7-v9s")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "21145186"))
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6011680723"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://infohubstore06:<password>@firstone.0pmqz95.mongodb.net/?retryWrites=true&w=majority&appName=Firstone")
+DB_NAME = os.environ.get("DATABASE_NAME", "firstone")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002170049626 -1002123546604"))
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002170049626 -100
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "HELLO {mention}\n\n I will share you the file that you want but first, you must click on the button/link.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
